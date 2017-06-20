e8a79c11ce46a2da73e8e000b7e878bac691d6ec
sarahmluo <sarah.mcbride925@gmail.com>
2017-06-20 10:46:15 -0400

    Initial check-in of module Lab01-SarahLuo

---
+++ Lab01-SarahLuo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab01SarahLuo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            String text1 = "Hello World";
+            String text2 = "Sarah Luo";
+
+            Console.WriteLine("{0} from {1}", text1, text2);
+        }
+    }
+}
