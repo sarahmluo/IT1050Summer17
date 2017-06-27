41385f685288ffee8763c37baba9d53f262d44c6
sarahmluo <sarah.mcbride925@gmail.com>
2017-06-27 10:19:21 -0400

    created project

--- Lab02-SarahLuo/Lab02-SarahLuo/Program.cs
+++ Lab02-SarahLuo/Lab02-SarahLuo/Program.cs
@@ -3,10 +3,24 @@
 namespace Lab02SarahLuo
 {
     class MainClass
-    {
+    {// comment
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = 0;
+            int num2 = 0;
+
+            Console.Write("Enter an integer: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter another integer: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The product of the numbers you entered is {0}", num1 * num2);
+
+            Console.WriteLine("Hello World!\nFrom Sarah Luo");
+
+            Console.WriteLine("Hello World! \t From Sarah Luo");
+
         }
     }
 }
---
+++ RepoTest/RepoTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RepoTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
