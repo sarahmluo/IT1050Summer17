4d0ad180c4e64355b30a5e481f47f27bddef2946
sarahmluo <sarah.mcbride925@gmail.com>
2017-07-16 21:59:59 -0400

    Adding Lab 5

---
+++ Dog/Dog/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dog
+{
+    class Dog
+    {
+        public string Name { get; set; }
+
+        public void bark(){
+            Console.WriteLine("{0} is Barking...", Name);
+        }
+
+        public void doTrick(String trickName){
+            Console.WriteLine("{0} is so smart!  {0} is doing a(n) {1}", Name, trickName);
+        }
+    }
+}
---
+++ Dog/Dog/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+            myDog.Name = "Fido";
+
+            myDog.bark();
+            myDog.doTrick("Fetch");
+        }
+    }
+}
