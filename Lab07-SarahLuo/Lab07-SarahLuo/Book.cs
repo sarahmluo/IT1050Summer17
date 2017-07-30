7a8c1612c743c25e9d62bf606036a154b8a33fd4
sarahmluo <sarah.mcbride925@gmail.com>
2017-07-30 19:17:39 -0400

    Adding Lab 7

---
+++ Lab07-SarahLuo/Lab07-SarahLuo/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Lab07SarahLuo
+{
+    public class Book
+    {
+        public String Title { get; set; }
+        public String Author { get; set; }
+        public int Year { get; set; }
+
+        public Book(){}
+           
+        public void Display(){
+            Console.WriteLine("{0}, by {1}", Title, Author);
+        }
+    }
+}
---
+++ Lab07-SarahLuo/Lab07-SarahLuo/BookTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab07SarahLuo
+{
+    class BookTest
+    {
+        public static void Main(string[] args)
+        {
+            Book myBook = new Book {Title = "1984", Author = "George Orwell", Year = 1949};
+            myBook.Display();
+
+        }
+    }
+}
