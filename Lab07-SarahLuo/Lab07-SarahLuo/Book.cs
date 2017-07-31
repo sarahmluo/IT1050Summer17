fc5d65603b0f53655f8b5330bbd7cb6dc83a4fa7
sarahmluo <sarah.mcbride925@gmail.com>
2017-07-30 20:32:47 -0400

    updating Book constructor

--- Lab07-SarahLuo/Lab07-SarahLuo/Book.cs
+++ Lab07-SarahLuo/Lab07-SarahLuo/Book.cs
@@ -7,7 +7,11 @@ namespace Lab07SarahLuo
         public String Author { get; set; }
         public int Year { get; set; }
 
-        public Book(){}
+        public Book(String title = "", String author = "", int year = 0){
+            this.Title = title;
+            this.Author = author;
+            this.Year = year;
+        }
            
         public void Display(){
             Console.WriteLine("{0}, by {1}", Title, Author);
